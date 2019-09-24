e25a8120906246fc5515e5a20327c7b64f50cd9f
Marcel Balint <blank_blankness@yahoo.com>
2019-09-24 07:14:01 -0400

    Added new item to the budget controller

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,13 @@ var budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
     var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
     var data = {
         allItems: {
@@ -22,7 +22,34 @@ var budgetController = (function () {
             exp: 0,
             input: 0
         }
-    }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //create new ID
+            if (data.allItems[type] > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //push it to data structure
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -70,12 +97,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItemFunction = function () {
-
+        var input, newItem;
         //get input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         //add item to budget controller
-
+        newItem = budgetController.addItem(input.type, input.description, input.value);
 
         //add item to UI
 
