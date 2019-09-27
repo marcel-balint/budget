17a5f3fc075dfee101908e8b9dfacfdf51f598b3
Marcel Balint <blank_blankness@yahoo.com>
2019-09-27 05:02:53 -0400

    Created responsiveness

--- assets/js/app.js
+++ assets/js/app.js
@@ -172,6 +172,12 @@ var UIController = (function () {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i)
+        }
+    };
+
     return {
         getInput: function () {
             return {
@@ -243,11 +249,6 @@ var UIController = (function () {
         displayPercentages: function (percentages) {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercentLabel);
-            var nodeListForEach = function (list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i)
-                }
-            };
 
             nodeListForEach(fields, function (current, index) {
                 if (percentages[index] > 0) {
@@ -269,6 +270,22 @@ var UIController = (function () {
 
         },
 
+        changedType: function () {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue + ',' +
+                DOMstrings.inputBtn
+            )
+
+            nodeListForEach(fields, function (cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -283,13 +300,14 @@ var controller = (function (budgetCtrl, UICtrl) {
     var setupEventListners = function () {
         var DOM = UICtrl.getDOMstrings();
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItemFunction);
-
         document.addEventListener('keypress', function (event) {
             if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItemFunction();
             }
         });
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function () {
@@ -302,7 +320,6 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     var updatePrecentages = function () {
-
         //calculate percentages
         budgetCtrl.calcuatePercentages();
 
